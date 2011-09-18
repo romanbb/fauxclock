7dad2fe90cef99730a15e28e85b2d60d063b8d8d
Roman Birg <romanbirg@gmail.com>
2011-09-18 01:21:19 -0500

    add Apache 2 License

--- src/com/teamkang/fauxclock/receiver/BootReceiver.java
+++ src/com/teamkang/fauxclock/receiver/BootReceiver.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2011 Roman Birg, Paul Reioux, RootzWiki
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package com.teamkang.fauxclock.receiver;
 
 import android.content.BroadcastReceiver;
@@ -11,23 +27,23 @@ import com.teamkang.fauxclock.cpu.CpuInterface;
 
 public class BootReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		Log.e("BootReceiver", "Booted, starting cpu reading!");
-		CpuInterface cpu = PhoneManager.getCpu(context);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("BootReceiver", "Booted, starting cpu reading!");
+        CpuInterface cpu = PhoneManager.getCpu(context);
 
-		if (cpu.getSettings().getBoolean("load_on_startup", false)
-				&& cpu.getSettings().getBoolean("safe", false)) {
-			cpu.loadValuesFromSettings();
+        if (cpu.getSettings().getBoolean("load_on_startup", false)
+                && cpu.getSettings().getBoolean("safe", false)) {
+            cpu.loadValuesFromSettings();
 
-			if (PhoneManager.supportsGpu()) {
-				GpuController gpu = new GpuController(context);
-				gpu.loadValuesFromSettings();
+            if (PhoneManager.supportsGpu()) {
+                GpuController gpu = new GpuController(context);
+                gpu.loadValuesFromSettings();
 
-			}
-		} else if (!cpu.getSettings().getBoolean("safe", false)) {
-			cpu.getEditor().clear().commit();
-		}
+            }
+        } else if (!cpu.getSettings().getBoolean("safe", false)) {
+            cpu.getEditor().clear().commit();
+        }
 
-	}
+    }
 }
